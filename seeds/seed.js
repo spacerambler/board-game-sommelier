bf7ff7e89d4f4a66b2c5795f7787526c02399be1
Zathras <ChampionMike42@gmail.com>
2021-08-10 20:14:56 -0500

    Add seed files

---
+++ seeds/seed.js
@@ -0,0 +1,23 @@
+const seedMechanics = require("./mechanicData.json");
+const seedGames = require("./gameData.json");
+const seedPlayers = require("./playerData.json");
+
+const sequelize = require("../config/connection");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  console.log("\n----- DATABASE SYNCED -----\n");
+
+  await seedMechanics();
+  console.log("\n----- MECHANICS SEEDED -----\n");
+
+  await seedGames();
+  console.log("\n----- GAMES SEEDED -----\n");
+
+  await seedPlayers();
+  console.log("\n----- PLAYERS SEEDED -----\n");
+
+  process.exit(0);
+};
+
+seedDatabase();

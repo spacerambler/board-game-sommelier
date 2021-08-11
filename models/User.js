0655130813959283cf3a8e8edd98322a22e91ef4
Jason Viox <jrv04@hotmail.com>
2021-08-10 20:43:48 -0500

    updated model DataTypes const

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes, STRING } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 

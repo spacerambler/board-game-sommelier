5ec5aca237b9d3f6da9b07091f5e46532bc7a0a2
Jason Viox <jrv04@hotmail.com>
2021-08-10 20:58:16 -0500

    finish player.js

---
+++ models/Player.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Player extends Model {}
+
+Player.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    mechanics: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "player",
+}
+);
+
+module.exports = Player;

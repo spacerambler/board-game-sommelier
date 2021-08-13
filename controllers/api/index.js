fd9248a0a5a71c947939639e057a8929e140de0c
Jason Viox <jrv04@hotmail.com>
2021-08-12 20:52:30 -0500

    add routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,24 @@
+<<<<<<< HEAD
 const router = require("express").Router();
 
+=======
+// eslint-disable-next-line new-cap
+const router = require("express").Router();
+>>>>>>> ba22a36882b817ba675c8f9c4200751bb7295e04
 const userRoutes = require("./userRoutes");
 const playerRoutes = require("./playerRoutes");
 const gameRoutes = require("./gameRoutes");
 const mechanicsRoutes = require("./mechanicsRoutes");
 
 router.use("/users", userRoutes);
+<<<<<<< HEAD
 router.use("/players", playerRoutes);
 router.use("games", gameRoutes);
 router.use("mechanics", mechanicsRoutes);
+=======
+router.use("/player", playerRoutes);
+router.use("/games", gameRoutes);
+router.use("/mechanics", mechanicsRoutes);
+>>>>>>> ba22a36882b817ba675c8f9c4200751bb7295e04
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes.js");
 
 router.use("/", homeRoutes);
+=======
+// eslint-disable-next-line new-cap
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+>>>>>>> ba22a36882b817ba675c8f9c4200751bb7295e04
 router.use("/api", apiRoutes);
 
 module.exports = router;

39d6db878027b3f58a302e59e699859f39ca7921
Keely Bailey <keelybailey3@gmail.com>
2021-08-12 20:19:16 -0500

    Add in user route code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line new-cap
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const playerRoutes = require("./playerRoutes");
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line new-cap
 const router = require("express").Router();
 const apiRoutes = require("./api");
 

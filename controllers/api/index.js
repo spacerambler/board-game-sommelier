d059afc7e45625733f35730f804c9b644751052c
Keely Bailey <keelybailey3@gmail.com>
2021-08-12 19:42:29 -0500

    Add index code for each route file

---
+++ controllers/api/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const playerRoutes = require("./playerRoutes");
+const gameRoutes = require("./gameRoutes");
+const mechanicsRoutes = require("./mechanicsRoutes");
+
+router.use("/users", userRoutes);
+router.use("/player", playerRoutes);
+router.use("/games", gameRoutes);
+router.use("/mechanics", mechanicsRoutes);
+
+module.exports = router;

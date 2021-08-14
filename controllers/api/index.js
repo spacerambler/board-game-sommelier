a4daa4a10753fecc826bef50d64fa7fa9aaa8cce
Jason Viox <jrv04@hotmail.com>
2021-08-14 13:59:20 -0500

    Uncomment gamesRoute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 const userRoutes = require("./userRoutes.js");
 const playerRoutes = require("./playerRoutes.js");
-// const gameRoutes = require("./gameRoutes.js");
+const gameRoutes = require("./gameRoutes.js");
 // const mechanicsRoutes = require("./mechanicsRoutes.js");
 
 router.use("/users", userRoutes);
 router.use("/players", playerRoutes);
-// router.use("/games", gameRoutes);
+router.use("/games", gameRoutes);
 // router.use("/mechanics", mechanicsRoutes);
 
 module.exports = router;

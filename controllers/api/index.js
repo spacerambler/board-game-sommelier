717c8577cce414afe1e968075cf4e99f76170fb4
Jason Viox <jrv04@hotmail.com>
2021-08-16 12:28:20 -0500

    Uncommented mechanics route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,11 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes.js");
 const playerRoutes = require("./playerRoutes.js");
 const gameRoutes = require("./gameRoutes.js");
-// const mechanicsRoutes = require("./mechanicsRoutes.js");
+const mechanicsRoutes = require("./mechanicsRoutes.js");
 
 router.use("/users", userRoutes);
 router.use("/players", playerRoutes);
 router.use("/games", gameRoutes);
-// router.use("/mechanics", mechanicsRoutes);
+router.use("/mechanics", mechanicsRoutes);
 
 module.exports = router;

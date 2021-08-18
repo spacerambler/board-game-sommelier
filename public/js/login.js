904c449a428bc516157138f190382e458544e030
Gabe <gabe.babelito@gmail.com>
2021-08-17 21:20:32 -0500

    fixed signup and login

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* global */
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -16,30 +14,8 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace("/profile");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
-
-  if (name && email && password) {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({ name, email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      document.location.replace("/");
+      // If successful, redirect the browser to the home page
+      document.location.replace("/homepage");
     } else {
       alert(response.statusText);
     }
@@ -49,7 +25,3 @@ const signupFormHandler = async (event) => {
 document
   .querySelector(".login-form")
   .addEventListener("submit", loginFormHandler);
-
-document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);

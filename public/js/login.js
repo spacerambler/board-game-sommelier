55d4e3ebf0fa6687f274026ed5c37f10f242e36c
Gabe <gabe.babelito@gmail.com>
2021-08-18 20:17:00 -0500

    tweaks

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,5 @@
+document.querySelector("#login-form");
+
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
@@ -7,7 +9,7 @@ const loginFormHandler = async (event) => {
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
@@ -22,6 +24,4 @@ const loginFormHandler = async (event) => {
   }
 };
 
-document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+document.addEventListener("submit", loginFormHandler);
